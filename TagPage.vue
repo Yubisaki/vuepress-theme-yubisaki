941115fb1a23b98366a55bb403b4dd55c9c7cb1c
bloss <1248212664@qq.com>
2018-07-23 09:52:00 +0800

    feat: tag group

---
+++ TagPage.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        123 tag
+    </div>
+</template>
+
+<script>
+
+</script>
--- ToolGroup.vue
+++ ToolGroup.vue
@@ -1,14 +1,16 @@
 <template>
   <div class="tool-group">
       <GithubCard />
+      <!-- <TagGroup /> -->
   </div>
 </template>
 
 <script>
     import GithubCard from './GithubCard.vue'
+    import TagGroup from './TagGroup'
     export default {
         name: 'tool-group',
-        components: { GithubCard }
+        components: { GithubCard, TagGroup }
     }
 </script>
 
