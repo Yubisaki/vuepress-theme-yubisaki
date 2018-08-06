f036eb7a7c7b53e7aee3d5cce7cf61d3761c2fb6
bloss <1248212664@qq.com>
2018-08-06 22:53:08 +0800

    feat: tags 页面初步完成

--- components/Tags.vue
+++ components/Tags.vue
@@ -1,20 +1,35 @@
 <template>
     <div>
         <h2 :style="overrideStyle">TAGS</h2>
-        <div>
+        <div class="tag-group">
             <Tag v-for="tag in Object.keys($tags)"
                 :tag="$tags[tag]"
                 :slug="tag"
-                :key="tag">#{{tag}} {{$tags[tag].length}}</Tag>
+                :key="tag">
+                #{{tag}} {{$tags[tag].length}}
+            </Tag>
         </div>
+        <ArticleGroup :page-items="pageItems" />
+        <Pagation 
+            :page-items="pageWithSpecTag"
+            @change="page => currentPage = page" />
     </div>
 </template>
 
 <script>
 import Tag from './Tag';
+import ArticleGroup from './ArticleGroup.vue';
+import Pagation from './Pagation.vue'
+import navLayoutMixin from '../lib/navLayout.mixin'
 
 export default {
-    components: { Tag },
+    mixins: [navLayoutMixin],
+    data() {
+        return {
+            currentPage: 1
+        }
+    },
+    components: { Tag, ArticleGroup, Pagation },
     computed: {
         overrideStyle() {
             const accentColor = this.$site.themeConfig['accentColor'];
@@ -22,9 +37,24 @@ export default {
         },
         tagName() {
             return this.$route.params.tagName;
-        }
+        },
+        pageItems() {
+            const start = (this.currentPage - 1) * this.perPage;
+            const end = this.currentPage * this.perPage;
+            return this.pageWithSpecTag.filter((page, i) => (i >= start && i < end));
+        },
+        pageWithSpecTag() {
+            if (!this.tagName) return [];
+            const tagKeys = this.$tags[this.tagName.toLowerCase()];
+            return this.pages.filter(page => page.key && !!~tagKeys.indexOf(page.key));
+        },
     }
 }
 </script>
 
 <style src="../styles/theme.styl" lang="stylus"></style>
+<style lang="stylus">
+.tag-group {
+    margin-bottom 1rem
+}
+</style>
