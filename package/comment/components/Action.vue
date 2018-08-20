6b31f59cbd76de21243114439d74038c8b0b47e7
bloss <1248212664@qq.com>
2018-08-21 01:07:33 +0800

    feat: comment system

---
+++ package/comment/components/Action.vue
@@ -0,0 +1,16 @@
+<template>
+    <a class='gt-action' @click="click">
+        <span class="gt-action-text">{{text}}</span>
+    </a>
+</template>
+
+<script>
+export default {
+    props: ['text'],
+    methods: {
+        click() {
+            this.$emit('click')
+        }
+    }
+}
+</script>
---
+++ package/comment/components/Button.vue
@@ -0,0 +1,19 @@
+<template>
+    <button
+        class='gt-btn'
+        @click="click">
+        <slot></slot>
+        <span v-if="isLoading" class="gt-btn-loading gt-spinner"></span>
+  </button>
+</template>
+
+<script>
+export default {
+    props: ['isLoading'],
+    methods: {
+        click() {
+            this.$emit('click')
+        }
+    }
+}
+</script>
