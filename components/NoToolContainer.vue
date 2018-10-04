72c6641f1c37de4ba2cf815f1238c0861f710279
bloss <1248212664@qq.com>
2018-10-05 00:57:38 +0800

    reactor: change ActivityContainer to NoToolContainer

---
+++ components/NoToolContainer.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <Navbar />
+    <div class="sidebar-mask" @click="togglesidebar(false)"></div>
+    <!-- <sidebar :items="sidebarItems" @toggle-sidebar="toggleSidebar"/> -->
+    <div class="layout-container">
+      <div class="main">
+        <slot />
+      </div>
+    </div>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Footer from '@theme/components/Footer';
+
+export default {
+  components: { Footer }
+}
+</script>
+
+<style src="prismjs/themes/prism-tomorrow.css"></style>
+<style src="../styles/theme.styl"></style>
+<style lang="stylus">
+@import '../styles/config.styl';
+.main
+  width $appWidth
+  padding 1rem
+  margin 0 auto
+
+.layout-container
+  margin-top $navbarHeight
+  margin-bottom $footerHeight
+
+@media (max-width: ($MQNarrow + 1px)) {
+  .main {
+    width 100%
+    padding 0 
+  }
+}
+</style>
+
